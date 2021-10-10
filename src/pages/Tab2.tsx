86551482591731313f1a2d556b12424c9dc5c5a2
iamsamdiak <thediakarthur@gmail.com>
2021-10-10 08:07:16 +0200

    first-commit

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonCol, IonRow, IonTitle, IonToolbar, IonItem, IonLabel } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import { BrowserRouter as Router, 
   Redirect, 
@@ -12,7 +12,6 @@ const Tab2 = () => {
 
   let { livreId } : any = useParams();
   let Bible = data[livreId - 1]; 
-  console.log(livreId);
 
   return (
     <IonPage>
@@ -23,13 +22,17 @@ const Tab2 = () => {
       </IonHeader>
       <IonContent fullscreen>
       <IonContent>
+      <IonRow>
         {Bible.chapitres.map((d, key) => {
         return (
-          <IonItem href={`/tab3/${livreId}/${livreId}-${Bible.livre}/${Bible.chapitres[key]}`} key={key}>
+          <IonCol size="3">
+          <IonItem className="chapFormat" href={`/tab3/${livreId}/${livreId}-${Bible.livre}/${Bible.chapitres[key]}`} key={key}>
             <IonLabel>{key+1}</IonLabel>
           </IonItem>
+          </IonCol>
         );
         })}
+      </IonRow>
       </IonContent>
       </IonContent>
     </IonPage>
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,19 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonText } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonText, IonRow, IonCol } from '@ionic/react';
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import ExploreContainer from '../components/ExploreContainer';
-import { useParams, useLocation } from 'react-router';
+import { useParams, useLocation, useHistory } from 'react-router';
 import data from "./../data/bible.json";
 import './Tab3.css';
+
+export let LastChap = "";
 
 const Tab3  = () => {
   let { livreId, livreDir, livreChap } : any = useParams();
   let Bible = data[livreId - 1]; 
   const [state, setState] = useState("");
+  let history = useHistory();
+  LastChap = useLocation().pathname;
+  const goToPreviousPath = () => {
+    history.goBack()
+  }
 
   useEffect(() => {
+    
     const url = `/livres/${livreDir}/${livreChap}`;
-
     const fetchData = async () => {
       try {
           const response = await fetch(url);
@@ -33,14 +39,21 @@ const Tab3  = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>{Bible.livre} - Chapitre { livreChap.replace( /^\D+/gm, '').replace(".txt", "") }</IonTitle>
+        <IonRow>
+          <IonCol onClick={goToPreviousPath} style={{ paddingLeft : "15px", fontWeight : "800" }} size="0">
+            &#8678;
+          </IonCol>
+          <IonCol>
+            <IonTitle>{Bible.livre} - Chapitre { livreChap.replace( /^\D+/gm, '').replace(".txt", "") }</IonTitle>
+          </IonCol>
+        </IonRow>    
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonText>{state}</IonText>
+        <IonText className="textFormat">{state}</IonText>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab3;
+export default Tab3; 
